5cedb4ace9c6d15829fa3b7a9e25b9448a52dda7
Raymon <rbasnyat666@gmail.com>
2023-03-17 11:41:29 +0545

    create navroutes

---
+++ src/routes/NavRoutes.tsx
@@ -0,0 +1,37 @@
+import LandingPage from "../pages/LandingPage";
+import LoginPage from "../pages/LoginPage";
+import MintPage from "../pages/MintPage";
+import ForgotPassword from "../pages/ForgotPassword";
+import RegisterPage from "../pages/RegisterPage";
+import ArchivesPage from "../pages/ArchivesPage";
+
+export const NavRoutes = [
+  {
+    path: "",
+    element: <LandingPage />,
+  },
+  {
+    path: "mint",
+    element: <MintPage />,
+  },
+  {
+    path: "login",
+    element: <LoginPage />,
+  },
+  {
+    path: "forgot",
+    element: <ForgotPassword />,
+  },
+  {
+    path: "register",
+    element: <RegisterPage />,
+  },
+  {
+    path: "mint",
+    element: <MintPage />,
+  },
+  {
+    path: "archive",
+    element: <ArchivesPage />,
+  },
+];
