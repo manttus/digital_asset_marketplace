1b4b8e8ad99587dbec6fc2cb6f96dd57966715be
manttus <rbasnyat666@gmail.com>
2023-04-29 20:17:21 +0545

    add chat feature

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,7 +1,20 @@
-import { Flex } from "@chakra-ui/react";
+import {
+  Avatar,
+  Flex,
+  Input,
+  Text,
+  Menu,
+  MenuList,
+  MenuButton,
+  Button,
+  HStack,
+  Icon,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import Details from "../components/Showcase/Details";
 import Profile from "../components/Showcase/Profile";
+import { RiSendPlaneFill } from "react-icons/ri";
+
 import {
   useUpdateMutation,
   useUserMutation,
@@ -14,16 +27,26 @@ import { selectUserData, setUserData } from "../features/auth/authSlice";
 import { RootState } from "../types/StoreType";
 import { useParams, useNavigate } from "react-router-dom";
 import useCustomToast from "../hooks/useToast";
+import { io } from "socket.io-client";
 
 const ProfilePage = () => {
+  const socket = io("http://localhost:3001");
   const id = useSelector((state: RootState) => state.auth.user);
   const [follow] = useFollowMutation();
   const [isEditPage, setIsEditPage] = useState<boolean>(false);
-  const [updateUser, { isLoading }] = useUpdateMutation();
+  const [updateUser] = useUpdateMutation();
+  const [message, setMessage] = useState<string>();
   const [profileData, setProfileData] = useState<any>();
+  const [isExpanded, setIsExpanded] = useState<boolean>(false);
+  const [messages, setMessages] = useState<any[]>([]);
+  const [messageId, setMessageId] = useState<{
+    id: string;
+    username: string;
+  } | null>(null);
   const [user] = useUserMutation();
   const cloudName = import.meta.env.VITE_CLOUD_NAME;
   const userData = useSelector(selectUserData);
+
   const dispatch = useDispatch();
   const { id: profileId } = useParams();
 
@@ -33,12 +56,27 @@ const ProfilePage = () => {
   const fetchProfileData = async (userId: string) => {
     try {
       const data = await user(userId).unwrap();
-      profileId === undefined && dispatch(setUserData({ user: data.user }));
       setProfileData(data.user);
     } catch (err) {
       showToast("Profile not found", "error", 2000);
       navigate("/profile");
     }
+  };
+
+  useEffect(() => {
+    socket.on("message", (data) => {
+      // Add the new message to your UI
+      setMessages((messages) => [...messages, data]);
+    });
+  }, []);
+
+  const sendMessages = async () => {
+    socket.emit("message", {
+      message,
+      senderId: id,
+      receiverId: messageId!.id,
+    });
+    setMessage("");
   };
 
   useEffect(() => {
@@ -103,13 +141,17 @@ const ProfilePage = () => {
           followId: profileId,
           status: "follow",
         }).unwrap();
+        showToast("Started Following", "success", 2000);
       } else {
         const response = await follow({
           id,
           followId: profileId,
           status: "unfollow",
         }).unwrap();
+
+        showToast("Unfollowed", "info", 2000);
       }
+
       const updateData = await user(id!).unwrap();
       dispatch(setUserData({ user: updateData.user }));
     } catch (err) {
@@ -118,22 +160,212 @@ const ProfilePage = () => {
   };
 
   return (
-    <Flex direction={"column"} px={"10"} py={"5"}>
+    <Flex direction={"column"} px={"10"}>
       <Profile addImage={AddImage} isEditPage={isEditPage} />
-      <Details
-        isEditPage={isEditPage}
-        setEditPage={setIsEditPage}
-        profileId={profileId}
-        profileData={profileData}
-        id={id!}
-        followHandler={followHandler}
-      />
-      {!profileId && (
-        <Flex w={"70%"} justifyContent={"center"}>
-          <FeedTabs />
+      <Flex w={"full"} direction={"column"} position={"relative"}>
+        <Details
+          isEditPage={isEditPage}
+          setEditPage={setIsEditPage}
+          profileId={profileId}
+          profileData={profileData}
+          id={id!}
+          followHandler={followHandler}
+        />
+        {!profileId && (
+          <Flex w={"100%"} justifyContent={"space-between"}>
+            <Flex w={"60%"} justifyContent={"center"}>
+              <FeedTabs />
+            </Flex>
+            <Flex
+              w={"25%"}
+              h={"400px"}
+              direction={"column"}
+              justifyContent={"end"}
+            >
+              {/* <Flex height={"300px"} w={"full"} boxShadow={"sm"}></Flex> */}
+            </Flex>
+          </Flex>
+        )}
+      </Flex>
+      <Flex justifyContent={"end"} position={"sticky"} bottom={0}>
+        <Flex
+          bg={"background"}
+          rounded={"md"}
+          cursor={"pointer"}
+          direction={"column"}
+          transition={"all 0.1s ease"}
+          w={"360px"}
+          borderX={"1px solid"}
+          borderTop={"1px solid"}
+          borderColor={"gray.300"}
+        >
+          <Menu autoSelect={false}>
+            <Flex
+              as={MenuButton}
+              h={"50px"}
+              px={"5"}
+              transition={"all 0.2s ease"}
+              onClick={() => {
+                setIsExpanded(!isExpanded);
+              }}
+            >
+              <Text fontWeight={"600"} fontSize={"16px"}>
+                Messages
+              </Text>
+            </Flex>
+            <MenuList
+              bg={"background"}
+              display={"flex"}
+              rounded={"md"}
+              overflow={"hidden"}
+              h={"328px"}
+              transition={"all 0.1s ease"}
+              w={"359px"}
+              borderColor={"gray.300"}
+              p={0}
+              flexDirection={"column"}
+              overflowY={!messageId ? "scroll" : "hidden"}
+              sx={{
+                "&::-webkit-scrollbar": {
+                  display: "none",
+                },
+              }}
+            >
+              {!messageId ? (
+                <>
+                  <Flex
+                    zIndex={"2"}
+                    position={"sticky"}
+                    top={0}
+                    px={"10px"}
+                    bg={"background"}
+                    h={"60px"}
+                    borderBottom={"1px solid"}
+                    borderColor={"gray.300"}
+                    alignItems={"center"}
+                    py={"8"}
+                  >
+                    <Input type={"text"} rounded={"sm"} bg={"white"} />
+                  </Flex>
+                  {userData?.following?.map((user: any) => (
+                    <Flex
+                      key={user.username}
+                      px={"10px"}
+                      bg={"background"}
+                      h={"60px"}
+                      borderBottom={"1px solid"}
+                      borderColor={"gray.300"}
+                      alignItems={"center"}
+                      py={"8"}
+                      gap={"2"}
+                      onClick={() => {
+                        setMessageId({
+                          id: user._id,
+                          username: user.username,
+                        });
+                      }}
+                    >
+                      <Avatar size={"sm"} />
+                      {user.username}
+                    </Flex>
+                  ))}
+                </>
+              ) : (
+                <>
+                  <Flex
+                    zIndex={"2"}
+                    position={"sticky"}
+                    top={0}
+                    px={"10px"}
+                    bg={"background"}
+                    h={"60px"}
+                    borderBottom={"1px solid"}
+                    borderColor={"gray.300"}
+                    alignItems={"center"}
+                    py={"8"}
+                    justifyContent={"space-between"}
+                  >
+                    <Button
+                      onClick={() => {
+                        setMessageId(null);
+                      }}
+                      fontSize={"12px"}
+                    >
+                      Go Back
+                    </Button>
+                    <Text fontWeight={"600"} fontSize={"16px"}>
+                      {messageId?.username}
+                    </Text>
+                  </Flex>
+                  <Flex height={"400px"} w={"full"} p={"2"}>
+                    <Flex
+                      direction={"column"}
+                      w={"full"}
+                      height={"400px"}
+                      background={"white"}
+                      overflowY={"scroll"}
+                      border={"1px solid"}
+                      borderColor={"gray.300"}
+                      sx={{
+                        "&::-webkit-scrollbar": {
+                          display: "none",
+                        },
+                      }}
+                      boxShadow={"md"}
+                    >
+                      {messages?.map((message: any) => (
+                        <Flex
+                          key={message._id}
+                          w={"full"}
+                          direction={"column"}
+                          p={"2"}
+                        >
+                          {message.message}
+                        </Flex>
+                      ))}
+                    </Flex>
+                  </Flex>
+                  <Flex
+                    zIndex={"2"}
+                    position={"sticky"}
+                    bottom={0}
+                    px={"10px"}
+                    bg={"background"}
+                    h={"50px"}
+                    alignItems={"center"}
+                    py={"8"}
+                  >
+                    <HStack w={"full"} spacing={2} p={0}>
+                      <Input
+                        type={"text"}
+                        rounded={"md"}
+                        bg={"white"}
+                        w={"380px"}
+                        value={message}
+                        onChange={(e) => {
+                          setMessage(e.target.value);
+                        }}
+                      />
+                      <Button bg={"buttonPrimary"}>
+                        <Icon
+                          as={RiSendPlaneFill}
+                          color={"white"}
+                          size={"22px"}
+                          type="submit"
+                          onClick={() => {
+                            console.log("send");
+                            sendMessages();
+                          }}
+                        />
+                      </Button>
+                    </HStack>
+                  </Flex>
+                </>
+              )}
+            </MenuList>
+          </Menu>
         </Flex>
-      )}
-      {}
+      </Flex>
     </Flex>
   );
 };
